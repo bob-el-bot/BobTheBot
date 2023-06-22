a79d0700cfd35a72bde5806fb95a75059bebd6a6
Zach Goodson <quantamstudios@gmail.com>
2023-06-21 19:48:59 -0500

    Update /fonts and /confess option display

--- classes-helpers/confess.cs
+++ classes-helpers/confess.cs
@@ -1,12 +1,17 @@
+using Discord.Interactions;
+
 public class Confession
 {
     public enum SignOffs
     {
         Anon,
+        [ChoiceDisplay("Secret Admirer")]
         Secret_Admirer,
+        [ChoiceDisplay("You know who")]
         You_Know_Who,
         Guess,
         FBI,
+        [ChoiceDisplay("Your Dad")]
         Your_Dad
     }
 }
