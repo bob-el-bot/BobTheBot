3f20e4bc962c11b9c0487ea7c410245626d59aef
Zach Goodson <58115931+Quantam-Studios@users.noreply.github.com>
2023-08-10 23:23:49 -0500

    #21 Add /quote group, add /quote new

--- commands.cs
+++ commands.cs
@@ -9,6 +9,7 @@ using Discord;
 using System.Collections.Generic;
 using System.Linq;
 using ColorHelper;
+using System.Diagnostics;
 
 public class Commands : InteractionModuleBase<SocketInteractionContext>
 {
@@ -617,6 +618,60 @@ public class Commands : InteractionModuleBase<SocketInteractionContext>
     {
         // Respond
         await RespondAsync(text: "🏰 Have an idea for a command? Share it on the official server for Bob The Bot.\nhttps://discord.gg/HvGMRZD8jQ");
+    }
+
+    [EnabledInDm(false)]
+    [Group("quote", "All quoting commands.")]
+    public class QuoteCommands : InteractionModuleBase<SocketInteractionContext>
+    {
+        [EnabledInDm(false)]
+        [SlashCommand("new", "Create a quote.")]
+        public async Task New([Summary("quote", "The text you want quoted. Quotation marks (\") will be added.")] string quote, [Summary("user", "The user who the quote belongs to.")] SocketUser user, [Summary("tag1", "A tag for sorting quotes later on.")] string tag1 = "", [Summary("tag2", "A tag for sorting quotes later on.")] string tag2 = "", [Summary("tag3", "A tag for sorting quotes later on.")] string tag3 = "")
+        {
+            // Date
+            var dateTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            // Fomrat Quote
+            string formattedQuote = quote;
+            if (quote[0] != '"' && quote[quote.Length - 1] != '"')
+            {
+                formattedQuote = "\"" + quote + "\"";
+            }
+
+            // Create embed
+            var embed = new Discord.EmbedBuilder
+            {
+                Title = $"{formattedQuote}",
+                Color = new Discord.Color(2895667),
+                Description = $"-{user.Mention}, <t:{dateTime}:R>"
+            };
+
+            // Footer
+            string footerText = "";
+            if (tag1 != "" || tag2 != "" || tag3 != "")
+            {
+                footerText += "Tag(s): ";
+                string[] tags = { tag1, tag2, tag3 };
+                for (int index = 0; index < tags.Length; index++)
+                {
+                    if (tags[index] != "")
+                    {
+                        footerText += tags[index];
+                        if (index < tags.Length - 1)
+                            footerText += ", ";
+                    }
+                }
+                footerText += " | ";
+            }
+            footerText += $"Quoted by {Context.User.GlobalName}";
+            embed.WithFooter(footer => footer.Text = footerText);
+
+            // Respond
+            await RespondAsync(text: $"🖊️ The quote: **{formattedQuote}**\n-{user.Mention}", ephemeral: true);
+
+            // Send quote in quotes channel of server
+            await Context.Channel.SendMessageAsync(embed: embed.Build());
+        }
     }
 
     [EnabledInDm(false)]
