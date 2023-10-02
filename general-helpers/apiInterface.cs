5b112dca808ed8cb8cf3c8c1bc37d08d2703eb90
Zach Goodson <58115931+Quantam-Studios@users.noreply.github.com>
2023-10-02 17:34:44 -0500

    #45 Refactor Stat Methods With General POST Method

--- general-helpers/apiInterface.cs
+++ general-helpers/apiInterface.cs
@@ -1,13 +1,14 @@
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 
 public static class APIInterface
 {
-    private static readonly ProductInfoHeaderValue productValue = new ProductInfoHeaderValue("BobTheBot", "1.0");
-    private static readonly ProductInfoHeaderValue commentValue = new ProductInfoHeaderValue("(+https://github.com/Quantam-Studios/BobTheBot)");
+    private static readonly ProductInfoHeaderValue productValue = new("BobTheBot", "1.0");
+    private static readonly ProductInfoHeaderValue commentValue = new("(+https://github.com/Quantam-Studios/BobTheBot)");
 
     public enum AcceptTypes
     {
@@ -24,17 +25,42 @@ public static class APIInterface
     public static async Task<string> GetFromAPI(string link, AcceptTypes accept)
     {
         // Formulate Request
-        var httpClient = new HttpClient();
+        HttpClient httpClient = new();
 
-        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, link);
+        HttpRequestMessage request = new(HttpMethod.Get, link);
+        MediaTypeWithQualityHeaderValue acceptValue = new((accept == AcceptTypes.application_json) ? "application/json" : (accept == AcceptTypes.text_plain) ? "text/plain" : "image/*");
+        
         request.Headers.UserAgent.Add(productValue);
         request.Headers.UserAgent.Add(commentValue);
-        var acceptValue = new MediaTypeWithQualityHeaderValue((accept == AcceptTypes.application_json) ? "application/json" : (accept == AcceptTypes.text_plain) ? "text/plain" : "image/*");
         request.Headers.Accept.Add(acceptValue);
 
         // Send Request (Get The Quote)
         var resp = await httpClient.SendAsync(request);
         // Read In Content
         return await resp.Content.ReadAsStringAsync();
+    }
+
+    /// <summary>
+    /// Sends a POST request to the specified URL.
+    /// <returns>Returns an HttpRepsonseCode.</returns>
+    /// <remarks>Only use this method for authenticated requests.</remarks>
+    /// </summary>
+    public static async Task<HttpStatusCode> PostToAPI(string link, string token, StringContent content)
+    {
+        // Formulate Request
+        HttpClient httpClient = new();
+
+        HttpRequestMessage request = new(HttpMethod.Post, link);
+        MediaTypeWithQualityHeaderValue acceptValue = new(content.Headers.ContentType.ToString());
+        
+        request.Headers.UserAgent.Add(productValue);
+        request.Headers.UserAgent.Add(commentValue);
+        request.Headers.Accept.Add(acceptValue);
+        request.Headers.Authorization = new AuthenticationHeaderValue(token);
+        request.Content = content;
+
+        // POST
+        var req = await httpClient.SendAsync(request);
+        return req.StatusCode;
     }
 }
