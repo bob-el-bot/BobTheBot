e0da3c12f89a0bb079088604ac36d32cf92f85bb
Zach Goodson <58115931+Quantam-Studios@users.noreply.github.com>
2023-12-05 17:36:35 -0600

    Update Creator's Notes

--- commands/no-group/commands.cs
+++ commands/no-group/commands.cs
@@ -214,7 +214,7 @@ namespace Commands
                 Color = Bot.theme
             };
 
-            embed.AddField(name: "🗒️ Creators Notes", value: "- 🖊️ *The best* quote system is out now :partying_face: :tada: .\n- Bob has a place on the 🌐 web! [bobthebot.net](https://bobthebot.net)\n- Stay 📺 tuned for some awesome updates!", inline: false).AddField(name: "✨ Latest Update", value: commitMessage, inline: true).AddField(name: ":calendar_spiral: Date", value: $"<t:{commitDateID}:f>", inline: true).AddField(name: "🔮 See What's In the Works", value: "[Road Map](https://github.com/orgs/bob-el-bot/projects/4)");
+            embed.AddField(name: "🗒️ Creator's Notes", value: "- `/random color` now has image previews! A big deal for the :crystal_ball: future of Bob!\n- `/analyze-link` has been added and now you can :detective: track where a link will take you before even clicking it (up to 4 redirects). On top of that, you can learn if the site has cookies, is a shortened URL, or even if it's a Rick roll.\n- :mega: `/announce` has been added and can make you fancy embeds.\n- Stay 📺 tuned for some awesome updates!", inline: false).AddField(name: "✨ Latest Update", value: commitMessage, inline: true).AddField(name: ":calendar_spiral: Date", value: $"<t:{commitDateID}:f>", inline: true).AddField(name: "🔮 See What's In the Works", value: "[Road Map](https://github.com/orgs/bob-el-bot/projects/4)");
 
             await RespondAsync(embed: embed.Build());
         }
