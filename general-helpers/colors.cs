ce16dc92d1c0a7114afc2762d9ae33bcebee968d
Zach Goodson <58115931+Quantam-Studios@users.noreply.github.com>
2024-03-19 00:51:00 -0500

    Add More Color Parsing Error Checking.

--- general-helpers/colors.cs
+++ general-helpers/colors.cs
@@ -1,14 +1,33 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Text.RegularExpressions;
+using Discord;
 
 namespace ColorMethods
 {
     public static class Colors
     {
-        public static string StringToHex(string input)
+        public static readonly Dictionary<string, string> colors = new() { { "red", "ED4245" }, { "orange", "FFA500" }, { "yellow", "FEE75C" }, { "green", "57F287" }, { "black", "23272A" }, { "pink", "EB459E" }, { "blue", "3498DB" }, { "grey", "95A5A6" }, { "gray", "95A5A6" }, { "white", "FFFFFF" }, { "purple", "8D52FD" } };
+
+        public static Color TryGetColor(string input)
         {
-            return WordToHex(input) ?? HexStringToHex(input);
+            Color finalColor;
+
+            try
+            {
+                finalColor = Convert.ToUInt32(StringToHex(input), 16);
+            }
+            catch
+            {
+                finalColor = 0;
+            }
+
+            return finalColor;
+        }
+
+        private static string StringToHex(string input)
+        {
+            return WordToHex(input) ?? HexStringToHex(input) ?? null;
         }
 
         private static string HexStringToHex(string input)
@@ -20,7 +39,7 @@ namespace ColorMethods
             }
 
             // Ensure the string has a valid length
-            if (input.Length % 2 != 0)
+            if (input.Length % 2 != 0 || input.Length > 6)
             {
                 return null;
             }
@@ -38,8 +57,6 @@ namespace ColorMethods
 
         private static string WordToHex(string input)
         {
-            Dictionary<string, string> colors = new() { { "red", "ED4245" }, { "orange", "FFA500" }, { "yellow", "FEE75C" }, { "green", "57F287" }, { "black", "23272A" }, { "pink", "EB459E" }, { "blue", "3498DB" }, { "grey", "95A5A6" }, { "gray", "95A5A6" }, { "white", "FFFFFF" }, { "purple", "8D52FD" } };
-
             string lowerInput = input.ToLowerInvariant();
 
             string match = null;
@@ -55,5 +72,27 @@ namespace ColorMethods
 
             return match;
         }
+
+        public static string GetSupportedColorsString()
+        {
+            StringBuilder colorString = new();
+
+            int count = 0;
+            foreach (var color in colors)
+            {
+                colorString.Append(color.Key);
+                if (++count < colors.Count - 1)
+                {
+                    colorString.Append(", ");
+                }
+                else if (count == colors.Count - 1)
+                {
+                    colorString.Append(", and ");
+                }
+            }
+
+            return colorString.ToString();
+        }
+
     }
 }
