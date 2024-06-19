36cd746e7daa302eb6609c57eab88b048bedfdb2
Zach Goodson <quantamstudios@gmail.com>
2024-06-18 20:28:43 -0500

    Update /premium Message

--- commands/no-group/commands.cs
+++ commands/no-group/commands.cs
@@ -792,7 +792,12 @@ namespace Commands
             bool isPremium = Premium.IsPremium(Context.Interaction.Entitlements);
             if (isPremium == false && Premium.IsValidPremium(user.PremiumExpiration) == false)
             {
-                await RespondWithPremiumRequiredAsync();
+                var components = new ComponentBuilder();
+                var premiumButton = new ButtonBuilder();
+                premiumButton.WithSkuId(1169107771673812992);
+                premiumButton.WithStyle(ButtonStyle.Premium);
+                components.WithButton(premiumButton);
+                await RespondAsync(text: "✨ You can get premium below!\n 💜 *Thanks so much!*", components: components.Build());
             }
             else
             {
