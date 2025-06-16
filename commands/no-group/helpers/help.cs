dfb23615e54bf74f0053b3d008ebe52f362c57c0
Zach Goodson <quantamstudios@gmail.com>
2025-06-15 22:25:52 -0500

    Make Docs include GIF and BMP in welcome set-image

--- commands/no-group/helpers/help.cs
+++ commands/no-group/helpers/help.cs
@@ -812,7 +812,7 @@ namespace Bob.Commands.Helpers
                             new ParameterInfo
                             {
                                 Name = "image",
-                                Description = "The image you would like to use (PNG, JPG, JPEG, WEBP). It will be compressed into a WEBP, but if the compressed image is larger than Discord's 8MB limit, you must compress it yourself or choose another image."
+                                Description = "The image you would like to use (PNG, JPG, JPEG, WEBP, GIF, BMP). It will be compressed into a WEBP, but if the compressed image is larger than Discord's 8MB limit, you must compress it yourself or choose another image."
                             }
                         ]
                     },
